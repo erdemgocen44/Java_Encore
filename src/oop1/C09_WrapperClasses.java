771dcfefb521db105d16fc70496167d5e7740392
Erdem <erdemgocen44@gmail.com>
2024-04-22 12:07:17 +0200

    Wrapper2

--- src/oop1/C09_WrapperClasses.java
+++ src/oop1/C09_WrapperClasses.java
@@ -11,7 +11,21 @@ public static void main(String[] args) {
 
     Integer sayi2=20; //bu data turu non primitive
 
-//Wrapper claslar aynı isimdeki primitive data türüyle geçişkenlik sağlar.
+//Wrapper claslar aynı isimdeki primitive data türüyle geçiş özelliğine sahiptir.
+
+Integer sayi3=sayi;
+
+/*boolen için Boolean 
+char için Character
+byte için Byte
+short için Short
+int için Integer
+long için Long
+float için Float
+double için Double
+
+*/
+
 
 
 
