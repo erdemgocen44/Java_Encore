5ebf7ba1ff34a43d5bc775f78efc93eb5f98d6b4
Erdem <erdemgocen44@gmail.com>
2024-04-22 12:13:04 +0200

    Wrapper3

--- src/oop1/C09_WrapperClasses.java
+++ src/oop1/C09_WrapperClasses.java
@@ -1,34 +1,44 @@
 package oop1;
 
 public class C09_WrapperClasses {
-public static void main(String[] args) {
-    int sayi=10;
+    public static void main(String[] args) {
+        int sayi = 10;
 
-    String str= "Java";
+        String str = "Java";
 
-    //Bazı Java Primitive data türleri için Wrapper Classları oluşturmuştur.
+        // Bazı Java Primitive data türleri için Wrapper Classları oluşturmuştur.
 
+        Integer sayi2 = 20; // bu data turu non primitive
 
-    Integer sayi2=20; //bu data turu non primitive
+        // Wrapper claslar aynı isimdeki primitive data türüyle geçiş özelliğine
+        // sahiptir.
 
-//Wrapper claslar aynı isimdeki primitive data türüyle geçiş özelliğine sahiptir.
+        /*
+         * boolen için Boolean
+         * char için Character
+         * byte için Byte
+         * short için Short
+         * int için Integer
+         * long için Long
+         * float için Float
+         * double için Double
+         * 
+         */
 
-Integer sayi3=sayi;
+        // Wrapper classlar sayesinde diğer data türleri içinde hazır methodlar
+        // kullanabiliriz...
 
-/*boolen için Boolean 
-char için Character
-byte için Byte
-short için Short
-int için Integer
-long için Long
-float için Float
-double için Double
+        Integer sayi3 = sayi;
 
-*/
+Scanner
 
+        String sayi5 = "23";
 
+        String sayi6 = "45";
 
+        int toplam = Integer.parseInt(sayi5) + Integer.parseInt(sayi6);
 
+        System.out.println(toplam);
 
-}
+    }
 }
