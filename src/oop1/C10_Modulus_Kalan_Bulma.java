98a2edc822df3d7df12698dfce9b8829b89aee18
Erdem <erdemgocen44@gmail.com>
2024-04-22 21:25:50 +0200

    Modulus c'est commence

--- src/oop1/C10_Modulus_Kalan_Bulma.java
+++ src/oop1/C10_Modulus_Kalan_Bulma.java
@@ -1,7 +1,19 @@
 package oop1;
 
+import java.util.Scanner;
+
 public class C10_Modulus_Kalan_Bulma {
-public static void main(String[] args) {
-    
-}
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen üç basamakli pozitif bir tamsayi giriniz: ");
+        int sayi = scanner.nextInt();
+
+        int sayi1 = sayi / 100;
+        int sayi2 = (sayi / 10) % 10;
+        int sayi3 = sayi % 10;
+
+        System.out.println("Vermis olduğunuz sayinin rakamlari toplami: " + sayi1 + sayi2 + sayi3);
+    }
 }
