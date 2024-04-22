ce8305860e05496dff42299e964c989b69f21369
Erdem <erdemgocen44@gmail.com>
2024-04-22 12:00:15 +0200

    Wrapper2

--- src/oop1/C09_WrapperClasses.java
+++ src/oop1/C09_WrapperClasses.java
@@ -2,6 +2,19 @@ package oop1;
 
 public class C09_WrapperClasses {
 public static void main(String[] args) {
-    
+    int sayi=10;
+
+    String str= "Java";
+
+    //Bazı Java Primitive data türleri için Wrapper Classları oluşturmuştur.
+
+
+    Integer sayi2=20; //bu data turu non primitive
+
+//Wrapper claslar aynı isimdeki primitive data türüyle geçişkenlik sağlar.
+
+
+
+
 }
 }
