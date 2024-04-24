361ca14af5dffc2345adb295e7fbd2d2bc2bda4f
Erdem <erdemgocen44@gmail.com>
2024-04-24 10:46:02 +0200

    preincrement c'est fini

--- src/oop1/C12_PreIncrement_PostIncrement.java
+++ src/oop1/C12_PreIncrement_PostIncrement.java
@@ -3,5 +3,37 @@ package oop1;
 public class C12_PreIncrement_PostIncrement {
 public static void main(String[] args) {
     
+int sayi=10;
+
+sayi++;
+
+System.out.println(sayi); // 11
+
+++sayi;
+
+System.out.println(sayi); // 12
+
+sayi--;
+
+System.out.println(sayi); // 11
+
+--sayi;
+
+System.out.print("Sayinin degeri: "+sayi); // 10
+
+sayi=10;
+
+System.out.println(sayi++); // 10
+// burada yazilan sonuc 10 olur 
+
+
+System.out.println(++sayi); // 12
+
+// sonra sayinin degeri 12 olur
+
+
+
+
+
 }
 }
