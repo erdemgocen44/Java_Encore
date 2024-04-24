06cd57da7ee6f64abd545d674ae710dc510888c9
Erdem <erdemgocen44@gmail.com>
2024-04-24 09:55:57 +0200

    sayı artırma ve azaltma

---
+++ src/oop1/C11_IncrementDecriment.java
@@ -0,0 +1,22 @@
+package oop1;
+
+public class C11_IncrementDecriment {
+    public static void main(String[] args) {
+        int sayi = 24;
+
+        // sayinin değerini artıralım
+
+        sayi = sayi + 5;
+
+        System.out.println(sayi);
+
+        sayi += 5;
+
+        System.out.println(sayi);
+
+        sayi++;
+
+        System.out.println(sayi);
+
+    }
+}
