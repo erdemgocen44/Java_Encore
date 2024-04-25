f8f22bab331448ee92d009313a3813d97fb1e576
Erdem <erdemgocen44@gmail.com>
2024-04-25 09:58:20 +0200

    relation c'est commence

--- src/oop1/C14_Relation.java
+++ src/oop1/C14_Relation.java
@@ -11,5 +11,22 @@ public class C14_Relation {
 
         System.out.println(a == b - 10);// true yazacak
 
+        System.out.println(sonuc = 15 == a);
+
+        System.out.println(a != b); // false yazacak
+
+        /*
+         * !true==>false
+         * !(5==5)==>false
+         * !(5!=5)==>true
+         * !(5>5)==>true
+         * !(5<5)==>false
+         * !(5>=5)==>false
+         * !(5<=5)==>true
+         * !(5!=5)==>true
+         * !(5==5)==>false
+         * 
+         */
+
     }
 }
