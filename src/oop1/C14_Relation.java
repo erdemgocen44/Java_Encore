c3dc8df73aff28f42b3295c206c7510446678c1d
Erdem <erdemgocen44@gmail.com>
2024-04-25 09:53:48 +0200

    relation c'est commence

--- src/oop1/C14_Relation.java
+++ src/oop1/C14_Relation.java
@@ -1,7 +1,15 @@
 package oop1;
 
 public class C14_Relation {
-public static void main(String[] args) {
-    
-}
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a > b);
+        boolean sonuc = a < b;
+        System.out.println(sonuc);
+
+        System.out.println(a == b - 10);// true yazacak
+
+    }
 }
