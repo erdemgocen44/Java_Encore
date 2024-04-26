95448c4c15cfcccfb227faff93e15145c392900f
Erdem <erdemgocen44@gmail.com>
2024-04-26 22:44:18 +0200

    if

---
+++ src/oop1/C16_If2.java
@@ -0,0 +1,15 @@
+package oop1;
+
+public class C16_If2 {
+    public static void main(String[] args) {
+        int a = 120;
+        int b = 30;
+
+        if (a < b) {
+            System.out.println("a sayisi b'den kucuktur.");
+        } else {
+            System.out.println("b sayisi a'dan kucuktur.");
+
+        }
+    }
+}
