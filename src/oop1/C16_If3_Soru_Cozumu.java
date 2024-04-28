dd64bcc98cc7b4b82881d848df1e92dfb49974f8
Erdem <erdemgocen44@gmail.com>
2024-04-28 14:17:02 +0200

    if soru çözümleri

---
+++ src/oop1/C16_If3_Soru_Cozumu.java
@@ -0,0 +1,17 @@
+package oop1;
+
+import java.util.Scanner;
+
+public class C16_If3_Soru_Cozumu {
+public static void main(String[] args) {
+    /*
+     * kullanıcıdan bir sayı isteyin, sayıyı kontrol edip
+     * 5 ile bölünüyorsa, 5 ile bölünen sayıyı ekrana yazdırın
+     * 5 ile bölünmüyorsa, ekrana "5 ile bölünmez" yazdırın
+     */
+
+     Scanner scanner=new Scanner(System.in);
+
+     
+}
+}
