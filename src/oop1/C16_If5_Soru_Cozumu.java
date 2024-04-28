f30fb92ee80846f2b585190f9890f23cb9dacffe
Erdem <erdemgocen44@gmail.com>
2024-04-28 15:32:46 +0200

    if soru çözümleri 3 tamam

--- src/oop1/C16_If5_Soru_Cozumu.java
+++ src/oop1/C16_If5_Soru_Cozumu.java
@@ -7,17 +7,22 @@ public class C16_If5_Soru_Cozumu {
         /*
          * Kullanicidan bir ucgenin 3 kenar uzunluğunu alin
          * ucken eskenar ise "Eskenar ucken yazdirin"
+         * javada 3lu karsılastirma olmaz
          */
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Ucgenin uc kenar uzunluklarini giriniz: ");
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
 
         if (a == b && a == c && b == c)
             System.out.println("Bu bir eskenar uckendir");
+        if (a == 0 || b == 0 || c == 0)
+            System.out.println("Girilen deger 0'dan buyuk olmali!!");
+        if (a < 0 || b < 0 || c < 0)
+            System.out.println("Girilen deger eksi (-) bir sayi olamaz!!");
         else
             System.out.println("Bu bir eskenar ucken degildir");
 
