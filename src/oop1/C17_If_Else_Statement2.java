bbbccf7a6fa9ea18008b98f12d1d1b5a1e4fa9dc
Erdem <erdemgocen44@gmail.com>
2024-04-29 11:23:50 +0200

    if else blocları4

--- src/oop1/C17_If_Else_Statement2.java
+++ src/oop1/C17_If_Else_Statement2.java
@@ -17,17 +17,17 @@ public class C17_If_Else_Statement2 {
         System.out.println("Lutfen bir tam sayi giriniz: ");
         int sayi = scanner.nextInt();
 
-        if (sayi % 3 == 0)
-            System.out.println(sayi + " sayisi 3'un katidir");
+        if (sayi % 3 == 0 && sayi % 5 == 0)
+            System.out.println(sayi + " sayisi super sayidir, yani hem 3'e hemde 5'e bolunebilir");
 
         else if (sayi % 5 == 0)
             System.out.println(sayi + " sayisi 5'in katidir");
 
-        else if (sayi % 3 == 0 && sayi % 5 == 0)
-            System.out.println(sayi + " sayisi super sayidir");
+        else if (sayi % 3 == 0)
+            System.out.println(sayi + " sayisi 3'un katidir");
 
         else
-            System.out.println(sayi + " sayisi Yaramaz Sayidir");
+            System.out.println(sayi + " sayisi yaramaz sayidir");
 
         scanner.close();
 
---
+++ src/oop1/C17_If_Else_Statement3.java
@@ -0,0 +1,9 @@
+package oop1;
+
+public class C17_If_Else_Statement3 {
+    public static void main(String[] args) {
+        /*
+         * Kullanicidan kilo(kg) ve boyunu (cm) isteyip
+         */
+    }
+}
