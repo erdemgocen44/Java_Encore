1fd9ab724d8d2f91cd3a933ffd6d0cddff08caa7
Erdem <erdemgocen44@gmail.com>
2024-04-30 10:55:30 +0200

    if else blocları musteri sorursu hata giderildi

--- src/oop1/C17_If_Else_Musteri_Sorusu.java
+++ src/oop1/C17_If_Else_Musteri_Sorusu.java
@@ -7,7 +7,7 @@ public class C17_If_Else_Musteri_Sorusu {
         /*
          * Kullanicidan aldigi urun adedi ve liste fiyatini alin
          * kullaniciya musteri karti olup olmadigini sorun
-         * Musteri karti varsa ve urunden fazla alirsa %20, yoksa %15 indirim yapin
+         * Musteri karti varsa ve 10 urunden fazla alirsa %20, yoksa %15 indirim yapin
          * Musteri karti yoksa 10 urunden fazla alirsa %15, yoksa %10 indirim yapin
          */
 
@@ -19,10 +19,27 @@ public class C17_If_Else_Musteri_Sorusu {
         System.out.println("Lütfen aldiginiz urun listesinin fiyatini giriniz: ");
         double listeFiyati = scanner.nextDouble();
 
-        System.out.println("Urun kartiniz var mi? Evet icin 'E' hayir 'H' giriniz: ");
-        boolean musteriKarti = scanner.nextBoolean();
+        System.out.println("Urun kartiniz var mi? \nEvet icin 'E' hayir 'H' giriniz: ");
+        char musteriKarti = scanner.next().toUpperCase().charAt(0);
+        if (musteriKarti == 'E') {
+            if (urunAdedi >= 10)
+                System.out.println(
+                        "Tebrikler %20 indirim kazandiniz\nKasa toplaminiz: " + (listeFiyati * urunAdedi * 0.8));
+            else {
+                System.out.println("%15 indirim kazandiniz\nKasa toplaminiz: "
+                        + listeFiyati * urunAdedi * 0.15);
+            }
+        } else if (musteriKarti == 'H') {
+            if (urunAdedi >= 10) {
+                System.out.println(
+                        "%15 indirim kazandiniz\nKasa toplaminiz: " + (listeFiyati * urunAdedi * 0.85));
+            } else
+                System.out.println("%10 indirim kazandiniz\nKasa toplaminiz: "
+                        + (listeFiyati * urunAdedi * 0.9));
 
-        
-
+        } else {
+            System.out.println("Lutfen gecerli bir karakter giriniz");
+        }
     }
+
 }
