12cbfb90bb50ac7f865e25e68dd04c05b91eea26
Erdem <erdemgocen44@gmail.com>
2024-05-01 20:15:54 +0200

    switch case2

--- src/oop1/C19_Switch_Statement2.java
+++ src/oop1/C19_Switch_Statement2.java
@@ -1,5 +1,7 @@
 package oop1;
 
+import java.util.Scanner;
+
 public class C19_Switch_Statement2 {
     public static void main(String[] args) {
         /*
@@ -10,6 +12,14 @@ public class C19_Switch_Statement2 {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Lütfen ISTQB harflerinden birini seciniz: ");
-        char harf = scanner.next().charAt(0);
+        char harf = scanner.next().toUpperCase().charAt(0);
+        switch (harf) {
+            case 'I' -> System.out.println("International");
+            case 'S' -> System.out.println("Software");
+            case 'T' -> System.out.println("Testing");
+            case 'Q' -> System.out.println("Qualifications");
+            case 'B' -> System.out.println("Board");
+            default -> System.out.println("Lutfen gecerli bir harf giriniz");
+        }
     }
 }
