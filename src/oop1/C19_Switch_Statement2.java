5c23810552bb0ae11c93563c3754376ffc6812c1
Erdem <erdemgocen44@gmail.com>
2024-05-01 20:00:10 +0200

    switch case2

--- src/oop1/C19_Switch_Statement2.java
+++ src/oop1/C19_Switch_Statement2.java
@@ -2,6 +2,14 @@ package oop1;
 
 public class C19_Switch_Statement2 {
     public static void main(String[] args) {
+        /*
+         * Kullanicidan ISTQB kisaltmasindan harfin anlamini ogrenmek
+         * istedigini alin ve girilen harfin karsiligini yazdirin
+         */
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lütfen ISTQB harflerinden birini seciniz: ");
+        char harf = scanner.next().charAt(0);
     }
 }
