a1de29923968e29175043bf5523560531f0bc636
Erdem <erdemgocen44@gmail.com>
2024-05-01 21:09:55 +0200

    string manupulation 1

--- src/oop1/C20_String_Manupulation1.java
+++ src/oop1/C20_String_Manupulation1.java
@@ -3,5 +3,19 @@ package oop1;
 public class C20_String_Manupulation1 {
     public static void main(String[] args) {
 
+        String str = "Java Candir";
+
+        // ilk harf icin
+
+        System.out.println(str.charAt(0));
+
+        // harfleri buyutelim
+
+        System.out.println(str.toUpperCase());
+
+        // harfleri kucultelim
+
+        System.out.println(str.toLowerCase());
+
     }
 }
