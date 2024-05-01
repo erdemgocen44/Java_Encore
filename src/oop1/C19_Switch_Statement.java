c5cb2246258660ba16bffe9f547b5098dd6d3986
Erdem <erdemgocen44@gmail.com>
2024-05-01 19:57:44 +0200

    switch case2

--- src/oop1/C19_Switch_Statement.java
+++ src/oop1/C19_Switch_Statement.java
@@ -11,49 +11,20 @@ public class C19_Switch_Statement {
         int ay = scanner.nextInt();
 
         switch (ay) {
-            case 1:
-                System.out.println("Ocak");
-                break;
+            case 1 -> System.out.println("Ocak");
+            case 2 -> System.out.println("Subat");
+            case 3 -> System.out.println("Mart");
+            case 4 -> System.out.println("Nisan");
+            case 5 -> System.out.println("Mayis");
+            case 6 -> System.out.println("Haziran");
+            case 7 -> System.out.println("Temmuz");
+            case 8 -> System.out.println("Agustos");
+            case 9 -> System.out.println("Eylul");
+            case 10 -> System.out.println("Ekim");
+            case 11 -> System.out.println("Kasim");
+            case 12 -> System.out.println("Aralik");
 
-            case 2:
-                System.out.println("Subat");
-                break;
-            case 3:
-                System.out.println("Mart");
-                break;
-            case 4:
-                System.out.println("Nisan");
-                break;
-            case 5:
-                System.out.println("Mayis");
-                break;
-            case 6:
-                System.out.println("Haziran");
-                break;
-            case 7:
-                System.out.println("Temmuz");
-                break;
-            case 8:
-                System.out.println("Agustos");
-                break;
-            case 9:
-                System.out.println("Eylul");
-                break;
-            case 10:
-                System.out.println("Ekim");
-                break;
-            case 11:
-                System.out.println("Kasim");
-                break;
-            case 12:
-                System.out.println("Aralik");
-
-                break;
-
-            default:
-                System.out.println("Lutfen gecerli bir sayi giriniz");
-
-                break;
+            default -> System.out.println("Lutfen gecerli bir sayi giriniz");
         }
 
     }
---
+++ src/oop1/C19_Switch_Statement2.java
@@ -0,0 +1,7 @@
+package oop1;
+
+public class C19_Switch_Statement2 {
+    public static void main(String[] args) {
+
+    }
+}
