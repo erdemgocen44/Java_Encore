ff863672e716e63c5251dae5a851130ee729d2fa
Erdem <erdemgocen44@gmail.com>
2024-05-06 09:36:51 +0200

    String manupulation Substring

--- src/oop1/C14_String_Manupulations/C14_Substring1.java
+++ src/oop1/C14_String_Manupulations/C14_Substring1.java
@@ -23,5 +23,13 @@ public class C14_Substring1 {
 
         System.out.println(str.substring(0, 4));
 
+        System.out.println(str.substring(7, 11).toUpperCase());
+
+        // System.out.println(str.substring(8, 5));// hata verir, javada geri vites yok
+
+        // son 3 harfi yazdirma
+
+        System.out.println(str.substring(str.length() - 3).toUpperCase());
+
     }
 }
