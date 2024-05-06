ef7272cdf5cac61723c5ce4970ece7234bdf617c
Erdem <erdemgocen44@gmail.com>
2024-05-06 08:59:58 +0200

    String manupulation devam

---
+++ src/oop1/C14_String_Manupulations/C14_Char_At.java
@@ -0,0 +1,12 @@
+package oop1.C14_String_Manupulations;
+
+public class C14_Char_At {
+
+    public static void main(String[] args) {
+        String str = "Java Canandir";
+
+        System.out.println(str.charAt(0)); // Java
+
+        System.out.println(str.charAt(6));
+    }
+}
