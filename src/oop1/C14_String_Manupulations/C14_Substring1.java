bab7add37b3ee3f66eeccde0ec479a794314935a
Erdem <erdemgocen44@gmail.com>
2024-05-06 09:12:19 +0200

    String manupulation Substring

--- src/oop1/C14_String_Manupulations/C14_Substring1.java
+++ src/oop1/C14_String_Manupulations/C14_Substring1.java
@@ -2,14 +2,26 @@ package oop1.C14_String_Manupulations;
 
 public class C14_Substring1 {
     public static void main(String[] args) {
-        // bize verilen stringin bir parçasini kullanabiliriz
-        //2 turlu kullanimi vardir
 
         String str = "Java Canandir";
 
+        /*
+         * bize verilen stringin bir parçasini kullanabiliriz
+         * 2 turlu kullanimi vardir
+         * 1-baslangic indexini verirseniz o noktadan sona kadar
+         * olan parcayi verir.
+         */
+        System.out.println(str.substring(3));
+
+        System.out.println(str.substring(7));
+
+        /*
+         * baslangic ve bitis indexini verirseniz
+         * bitis indexi haric
+         * aradaki karakterleri yazdirir
+         */
+
         System.out.println(str.substring(0, 4));
-
-
 
     }
 }
