8171ad52bbfd651023ea2e6fff908c1ae0b027b6
Erdem <erdemgocen44@gmail.com>
2024-05-07 19:59:32 +0200

    null pointer 1

--- src/oop1/C14_String_Manupulations/C14_nullPointer.java
+++ src/oop1/C14_String_Manupulations/C14_nullPointer.java
@@ -4,5 +4,21 @@ public class C14_nullPointer {
     public static void main(String[] args) {
         String str1;
 
+        /*
+         * null bir deger degil, pointer dır
+         * yani isaretleyici
+         * str2'nin degeri null veya str2ye null degeri atanmis
+         * ifadeleri yanlistir. dogru olan ifade
+         * str2 null olarak ISARETLENMISTIR
+         * Ça ve dire : str2ye deger atamadik
+         * null olarak isaretledik
+         * deger atamayinca ne yazdirabiliriz nede baska islem yapabiliriz
+         */
+
+        String str2 = null;
+
+        System.out.println(str2);
+        System.out.println(str2 + " candir");// null candir
+
     }
 }
