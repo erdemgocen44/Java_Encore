c57a70301a527315819f3082e51961383b79336b
Erdem <erdemgocen44@gmail.com>
2024-05-08 10:38:09 +0200

    remplace 2

--- src/oop1/C15_forLoop/C15_replace.java
+++ src/oop1/C15_forLoop/C15_replace.java
@@ -7,5 +7,20 @@ public class C15_replace {
         System.out.println(str);// şimdi tum i yerine e yazalim degistirelim
         System.out.println(str.replace("i", "e"));// Fransezcaye anleyorum ama eye konusameyorum
 
+        // Sayilarin tamamindan kurtulmak istersek
+        // replace() yeterli olmaz
+        // java da sayilar, ozel karakterler, alfabetik karakterlerin // tumuyle islem
+        // yapmak isterseniz
+        // replaceAll(istenenGrup, yeniDeger)
+        // ornegin butun sayilarin yerine "" atamak icin
+
+        String str2 = "1Ja4va 8C9a5n7d11ir125";
+
+        System.out.println(str2);
+
+        str = str2.replaceAll("\\d", "");
+
+        System.out.println(str);
+
     }
 }
