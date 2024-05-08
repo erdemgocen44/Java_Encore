4dc7aaa972eabf9449b10c37bb6b8b3cd5c1a6a0
Erdem <erdemgocen44@gmail.com>
2024-05-08 11:08:10 +0200

    remplaceAll soru

--- src/oop1/C15_forLoop/C15_replaceAll_Soru.java
+++ src/oop1/C15_forLoop/C15_replaceAll_Soru.java
@@ -1,7 +1,17 @@
 package oop1.C15_forLoop;
 
+import java.util.Scanner;
+
 public class C15_replaceAll_Soru {
     public static void main(String[] args) {
+        /*
+         * kullanicidan bir metin isteyin
+         * metnin icindeki tum sayilari ve space disindaki
+         * ozel karakterleri temizleyin
+         */
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen bir metin giriniz: ");
     }
 }
