fe536d7801425e76cc5e44a8a6a63b42358c9787
Erdem <erdemgocen44@gmail.com>
2024-05-08 10:18:41 +0200

    remplace 1

--- src/oop1/C15_forLoop/C15_replace.java
+++ src/oop1/C15_forLoop/C15_replace.java
@@ -2,6 +2,10 @@ package oop1.C15_forLoop;
 
 public class C15_replace {
     public static void main(String[] args) {
+        String str = "Fransizcayi anliyorum ama iyi konusamiyorum";
+
+        System.out.println(str);// şimdi tum i yerine e yazalim degistirelim
+        System.out.println(str.replace("i", "e"));// Fransezcaye anleyorum ama eye konusameyorum
 
     }
 }
