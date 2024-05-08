59ce91c34175558cf3a26ba07c116147b21b7c1f
Erdem <erdemgocen44@gmail.com>
2024-05-08 11:26:57 +0200

    remplaceAll soru calisti

--- src/oop1/C15_forLoop/C15_replaceAll_Soru.java
+++ src/oop1/C15_forLoop/C15_replaceAll_Soru.java
@@ -13,5 +13,28 @@ public class C15_replaceAll_Soru {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Lutfen bir metin giriniz: ");
+
+        String girilenMetin = scanner.nextLine();
+        // once sayilardan kurtulalim
+        girilenMetin = girilenMetin.replaceAll("\\d", "");
+        /*
+         * once sayilardan kurtulduktan sonra space yerine herhangi bir sayi atayalim
+         * ozel karakterlerden kurtulmak istedigimizde
+         * space de kaybolur
+         * bunun icin once space i korumaya almaliyiz
+         * zaten replaceAll ın mantigi BUTUNNN olanlari yoket demektir
+         */
+
+        // ozel karakterlerden kurtulalim
+        girilenMetin = girilenMetin.replaceAll("\\d", "");
+
+        // simdi space yerine 1 ekleyelim
+        girilenMetin = girilenMetin.replaceAll("\\s", "1");
+
+        // simdi 1 lerden kurtulalim
+        girilenMetin = girilenMetin.replaceAll("1", " ");
+
+        System.out.println("Metninizin yeni hali su sekildedir: " + girilenMetin);
+
     }
 }
