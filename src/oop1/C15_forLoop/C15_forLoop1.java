fe4f51d108ff32ea0548b9ea6ab947af5bae79d8
Erdem <erdemgocen44@gmail.com>
2024-05-10 21:54:57 +0200

    forLoop 1

--- src/oop1/C15_forLoop/C15_forLoop1.java
+++ src/oop1/C15_forLoop/C15_forLoop1.java
@@ -3,5 +3,10 @@ package oop1.C15_forLoop;
 public class C15_forLoop1 {
     public static void main(String[] args) {
 
+        int input = 20;
+
+        for (int i = input; i < input + 10; i++) {
+            System.out.print(i + " ,");
+        }
     }
 }
