421b5637e34fb64031dd7078494526a4918ee0be
Erdem <erdemgocen44@gmail.com>
2024-05-10 22:34:03 +0200

    forLoop 1

--- src/oop1/C15_forLoop/C15_forLoop1.java
+++ src/oop1/C15_forLoop/C15_forLoop1.java
@@ -1,19 +1,22 @@
 package oop1.C15_forLoop;
 
-import java.util.Scanner;
-
 public class C15_forLoop1 {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        // Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Lutfen 5'er 5'er artirmak icin bir sayi giriniz: ");
+        // System.out.println("Lutfen 5'er 5'er artirmak icin bir sayi giriniz: ");
 
-        int girilenSayi = scanner.nextInt();
+        // int girilenSayi = scanner.nextInt();
 
-        for (int i = girilenSayi; i <= 100; i += 5) {
-            System.out.print(i);
+        // for (int i = girilenSayi; i <= 100; i += 5)
+        // System.out.print(i + " ,");
+
+        for (int i = 100; i >= 1; i--) {
+            if (i % 7 == 0)
+                System.out.print(i + ", ");
 
         }
+
     }
 }
