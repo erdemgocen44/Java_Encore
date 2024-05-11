10dd96c2c4de755f3abed15270d9a673ed9723bf
Erdem <erdemgocen44@gmail.com>
2024-05-11 20:49:24 +0200

    forLoop dikkat edilecekler

---
+++ src/oop1/C15_forLoop/C15_forLoopDikkatEdilecekler.java
@@ -0,0 +1,12 @@
+package oop1.C15_forLoop;
+
+public class C15_forLoopDikkatEdilecekler {
+    public static void main(String[] args) {
+        // Verdigimiz baslangic degeri bitis sartini karsilamazsa
+
+        for (int i = 0; i > 10; i++) {
+
+            System.out.println(i);
+        }
+    }
+}
