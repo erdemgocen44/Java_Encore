0dfd8e326bdf6a4944ea83540b9430e16372f40e
Erdem <erdemgocen44@gmail.com>
2024-05-11 20:55:44 +0200

    forLoop 1

--- src/oop1/C15_forLoop/C15_forLoopDikkatEdilecekler.java
+++ src/oop1/C15_forLoop/C15_forLoopDikkatEdilecekler.java
@@ -2,11 +2,20 @@ package oop1.C15_forLoop;
 
 public class C15_forLoopDikkatEdilecekler {
     public static void main(String[] args) {
-        // Verdigimiz baslangic degeri bitis sartini karsilamazsa
+        // 1) Verdigimiz baslangic degeri bitis sartini karsilamazsa
 
         for (int i = 0; i > 10; i++) {
 
-            System.out.println(i);
+            System.out.println(i);// exitcode 0 hatasi ile forloop calisir ama forloop hic DEVREYE GIRMEZ
+        }
+
+        // 2) baslangic degeri ve degisim sekli
+        // hicbir zaman bitis degerine ulasamazsa
+        // cunku sonsuz loop olusur
+        for (int i = 0; i >= 10; i++) {
+
+            System.out.println(i + ", ");
+
         }
     }
 }
