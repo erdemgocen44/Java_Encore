a14a3559e2cb71198f254837798f14439feca0a2
Erdem <erdemgocen44@gmail.com>
2024-05-12 21:48:40 +0200

    forLoop soru

---
+++ src/oop1/C15_forLoop/C15_forLoop_Soru.java
@@ -0,0 +1,11 @@
+package oop1.C15_forLoop;
+
+public class C15_forLoop_Soru {
+public static void main(String[] args) {
+//     Soru 5: Kullanicidan isim ve soyismini ayri ayri alin.
+// - ismi daha uzun ise, isim ve soyismi ilk harf buyuk kalanlar kucuk seklinde yazdirin
+// soyisim daha uzun ise ismi ilk harf buyuk digerleri kucuk, soyismi buyuk harflerle yazdirin.
+
+
+}
+}
