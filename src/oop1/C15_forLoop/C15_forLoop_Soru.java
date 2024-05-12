bb0131b4cdec4224aa216676103c1e47c68738a9
Erdem <erdemgocen44@gmail.com>
2024-05-12 21:51:10 +0200

    forLoop soru

--- src/oop1/C15_forLoop/C15_forLoop_Soru.java
+++ src/oop1/C15_forLoop/C15_forLoop_Soru.java
@@ -1,11 +1,22 @@
 package oop1.C15_forLoop;
 
+import java.util.Scanner;
+
 public class C15_forLoop_Soru {
-public static void main(String[] args) {
-//     Soru 5: Kullanicidan isim ve soyismini ayri ayri alin.
-// - ismi daha uzun ise, isim ve soyismi ilk harf buyuk kalanlar kucuk seklinde yazdirin
-// soyisim daha uzun ise ismi ilk harf buyuk digerleri kucuk, soyismi buyuk harflerle yazdirin.
+    public static void main(String[] args) {
+        // Soru 5: Kullanicidan isim ve soyismini ayri ayri alin.
+        // - ismi daha uzun ise, isim ve soyismi ilk harf buyuk kalanlar kucuk seklinde
+        // yazdirin
+        // soyisim daha uzun ise ismi ilk harf buyuk digerleri kucuk, soyismi buyuk
+        // harflerle yazdirin.
 
+        Scanner scanner = new Scanner(System.in);
 
-}
+        System.out.println("Lutfen isminizi giriniz: ");
+        String isim = scanner.nextLine();
+
+        System.out.println("Lutfen soyisminizi giriniz: ");
+        String soyisim = scanner.nextLine();
+
+    }
 }
