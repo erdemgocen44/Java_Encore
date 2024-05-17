1b88cb77c12044c1db557fc870211ed54e48507d
Erdem <erdemgocen44@gmail.com>
2024-05-17 21:31:16 +0200

    forLoop sayilarin rakamlari toplami sorusu

---
+++ src/oop1/C15_forLoop/C15_forLoop_Sayilarin_Rakamlarinin_Toplami.java
@@ -0,0 +1,11 @@
+package oop1.C15_forLoop;
+
+public class C15_forLoop_Sayilarin_Rakamlarinin_Toplami {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen rakamlari toplami icin bir sayi giriniz: ");
+
+        int sayi = scanner.nextInt();
+    }
+}
