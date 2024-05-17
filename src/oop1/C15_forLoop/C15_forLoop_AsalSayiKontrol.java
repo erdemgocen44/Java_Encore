0edd4d7e0a1a25719744804cbb3aa5b782246b41
Erdem <erdemgocen44@gmail.com>
2024-05-18 00:23:33 +0200

    forLoop asal sayi kontrol  sorusu

---
+++ src/oop1/C15_forLoop/C15_forLoop_AsalSayiKontrol.java
@@ -0,0 +1,9 @@
+package oop1.C15_forLoop;
+
+public class C15_forLoop_AsalSayiKontrol {
+    public static void main(String[] args) {
+        // Kullanicidan positif bir tamsayi isteyin
+        // sayinin asal olup olmadigini kontrol edin ve sonucu yazdirin
+
+    }
+}
--- src/oop1/C15_forLoop/C15_forLoop_Stringi_Ters_Cevir.java
+++ src/oop1/C15_forLoop/C15_forLoop_Stringi_Ters_Cevir.java
@@ -14,5 +14,12 @@ public class C15_forLoop_Stringi_Ters_Cevir {
         for (int i = str.length() - 1; i >= 0; i--) {
             System.out.print(str.charAt(i));
         }
+
+        // terse cevrilmis halini kaydedelim.
+        String tersStr = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            tersStr += str.charAt(i);
+        }
     }
 }
