e5d8f643f6c5481a574813409818deeb1ba1f7c8
Erdem <erdemgocen44@gmail.com>
2024-05-17 21:16:47 +0200

    forLoop faktoriyerl sorusu

---
+++ src/oop1/C15_forLoop/C15_forLoop_Factoriel_Sorusu.java
@@ -0,0 +1,14 @@
+package oop1.C15_forLoop;
+
+import java.util.Scanner;
+
+public class C15_forLoop_Factoriel_Sorusu {
+    public static void main(String[] args) {
+
+        // Kullanicidan 20 den kucuk bir sayi alip
+        // bu sayinin factoriyel degerini hesaplayalım
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
