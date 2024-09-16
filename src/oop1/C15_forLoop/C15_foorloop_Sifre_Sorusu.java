66139602e4dcdc6bc8217f815dcc5a8bf0b7d9c6
Erdem <erdemgocen44@gmail.com>
2024-09-16 21:08:46 +0200

    deuxieme

---
+++ src/oop1/C15_forLoop/C15_foorloop_Sifre_Sorusu.java
@@ -0,0 +1,15 @@
+package oop1.C15_forLoop;
+
+public class C15_foorloop_Sifre_Sorusu {
+    public static void main(String[] args) {
+
+        /*
+         * Kullanicidan isim ve soyisim ayrı ayrı girmesini isteyin
+         * ismi daha uzun ise isim ve soyismi ilk harf büyük kalanlar küçük sekilde
+         * yazdirin
+         * soyisim daha uzun ise ismi ilk harf buyuk digerleri kucuk
+         * soyismi buyuk harfle yazdirin
+         */
+
+    }
+}
