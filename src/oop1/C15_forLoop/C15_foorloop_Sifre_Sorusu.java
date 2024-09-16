3999e3ab7c41e1cff634ee9944f555874590ece0
Erdem <erdemgocen44@gmail.com>
2024-09-16 21:23:16 +0200

    deneme

--- src/oop1/C15_forLoop/C15_foorloop_Sifre_Sorusu.java
+++ src/oop1/C15_forLoop/C15_foorloop_Sifre_Sorusu.java
@@ -1,5 +1,7 @@
 package oop1.C15_forLoop;
 
+import java.util.Scanner;
+
 public class C15_foorloop_Sifre_Sorusu {
     public static void main(String[] args) {
 
@@ -10,6 +12,12 @@ public class C15_foorloop_Sifre_Sorusu {
          * soyisim daha uzun ise ismi ilk harf buyuk digerleri kucuk
          * soyismi buyuk harfle yazdirin
          */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen sirasiyla isim ve soyisminizi giriniz : ");
+        String isim = scanner.nextLine();
+        String soyisim = scanner.nextLine();
+        
 
     }
 }
