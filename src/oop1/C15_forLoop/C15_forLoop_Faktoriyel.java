cbf5370eebb0a2e39ab66196e9d7784b9190cc61
Erdem <erdemgocen44@gmail.com>
2024-09-19 10:24:52 +0200

    faktoriyel sorusu

---
+++ src/oop1/C15_forLoop/C15_forLoop_Faktoriyel.java
@@ -0,0 +1,10 @@
+package oop1.C15_forLoop;
+
+public class C15_forLoop_Faktoriyel {
+    public static void main(String[] args) {
+        /*
+         * Kullanicidan 20'den kucuk bir sayi alip,
+         * bu sayinin faktoryel degerini hesaplayin.
+         */
+    }
+}
