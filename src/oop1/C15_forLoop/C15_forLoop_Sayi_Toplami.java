90bfbfb8c19b1d009c9e8df383685e7e4d819708
Erdem <erdemgocen44@gmail.com>
2024-09-19 10:08:54 +0200

    sayi toplami sorusu

---
+++ src/oop1/C15_forLoop/C15_forLoop_Sayi_Toplami.java
@@ -0,0 +1,13 @@
+package oop1.C15_forLoop;
+
+public class C15_forLoop_Sayi_Toplami {
+    public static void main(String[] args) {
+        /*
+         * Kullanicidan baslangic ve bitis degeri olarak pozitif sayilar alin,
+         * sinirlar dahil olarak aralarındaki tum sayilarin toplamini yazdirin.
+         * Bitis degeri baslangic degerinden kucukse,
+         * uyari yazdirip islemi sonlandirin
+         */
+
+    }
+}
