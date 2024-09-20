87742107b95eebc204e1a55dd8a9aaf064e4e3e6
Erdem <erdemgocen44@gmail.com>
2024-09-20 20:18:36 +0200

    sayinin rakamlari toplami bitti

--- src/oop1/C15_forLoop/C15_forLoop_Sayilari_Topla.java
+++ src/oop1/C15_forLoop/C15_forLoop_Sayilari_Topla.java
@@ -1,22 +1,34 @@
 package oop1.C15_forLoop;
 
+import java.util.Scanner;
+
 public class C15_forLoop_Sayilari_Topla {
-public static void main(String[] args) {
-    /* Kullanicidan pozitif bir tamsayi alip, rakamlar toplamini yazdirin.
-     * Orn : 6543 ==> 1+6+5+4+3 = 16
-     */
+    public static void main(String[] args) {
+        /*
+         * Kullanicidan pozitif bir tamsayi alip, rakamlar toplamini yazdirin.
+         * Orn : 6543 ==> 1+6+5+4+3 = 16
+         */
 
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Lutfen pozitif bir sayi giriniz: ");
+        System.out.println("Lutfen pozitif bir sayi giriniz: ");
 
-    int sayi = scanner.nextInt();
+        int girilenSayi = scanner.nextInt();
 
-    int toplam=0;
+        int sayi = girilenSayi;
 
-    for(int i =1; i<=sayi; i++) {
+        int birlerBasamagi = 0;
 
-        toplam += i;
-}
-System.out.println("Verdiginiz sayinin rakamlari toplami : "+ toplam);
+        int rakamlarToplami = 0;
+
+        int basamakSayisi = (sayi + "").length();
+
+        for (int i = 1; i <= basamakSayisi; i++) {
+            birlerBasamagi = sayi % 10;
+            rakamlarToplami += birlerBasamagi;
+            sayi /= 10;
+        }
+
+        System.out.println("Verdiginiz " + girilenSayi + " sayisinin rakamlari toplami : " + rakamlarToplami);
+    }
 }
