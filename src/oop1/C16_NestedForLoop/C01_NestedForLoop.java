e312a6c17a30843cbe3da5f7f4a9b6e31e03f00f
Erdem <erdemgocen44@gmail.com>
2024-09-23 11:05:51 +0200

    nested for loop başlangıç

--- src/oop1/C16_NestedForLoop/C01_NestedForLoop.java
+++ src/oop1/C16_NestedForLoop/C01_NestedForLoop.java
@@ -1,7 +1,27 @@
 package oop1.C16_NestedForLoop;
 
 public class C01_NestedForLoop {
-public static void main(String[] args) {
-    
-}
+    public static void main(String[] args) {
+        /*
+         * Aşagidaki tabloyu konsolda yazdirin
+         * 
+         * 1 2 3 4
+         * 2 4 6 8
+         * 3 6 9 12
+         */
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.print(i + "  ");
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.print(i * 2 + "  ");
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.print(i * 3 + "  ");
+        }
+    }
 }
